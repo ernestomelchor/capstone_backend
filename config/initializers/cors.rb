34d7207a1fd03f96f8f4b2bedb15424e2ae061c9
ernestomelchor <nurturelove88@gmail.com>
2020-06-17 22:38:47 -0500

    slight mod to netlify url, removed https

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://epic-williams-393f0d.netlify.app/"
+    origins "localhost:8080", "epic-williams-393f0d.netlify.app/"
 
     resource "*",
       headers: :any,
