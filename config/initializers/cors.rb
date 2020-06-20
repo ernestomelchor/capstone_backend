20a47fe116f635f0f370b897933c375937726586
ernestomelchor <nurturelove88@gmail.com>
2020-06-20 18:48:47 -0500

    config CORS to accept new custom domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "https://chicagosoccerapp.netlify.app"
+    origins "localhost:8080", "https://chicagosoccerapp.netlify.app", "https://www.chicagosoccer.app", "https://chicagosoccer.app"
 
     resource "*",
       headers: :any,
